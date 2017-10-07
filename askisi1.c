134f28c476e7364bcec95bfd4dda5f745c1ccc5c
trakaros <caxelos@inf.uth.gr>
2017-10-07 17:26:04 +0300

    ask1

--- askisi1.c
+++ askisi1.c
@@ -7,8 +7,8 @@
 
 struct buffinfo {
   int size;
-  int rdptr;
-  int wrptr;
+  volatile int rdptr;
+  volatile int wrptr;
 };
 typedef struct buffinfo *bufferT;
 
@@ -50,21 +50,21 @@ void pipe_write(char c)  {
  
   int nxt_wr_ptr = (buff->wrptr + 1)%buff->size;
 
- //vres pou prepei na valeis ta race conditions  
   while (1)  {
     if ( nxt_wr_ptr != buff->rdptr )  {//check if it is full
-      buff->wrptr = nxt_wr_ptr;
       break;
     }
     printf("write: thread waiting\n");
   }   
-    
+  
+  //arxi krisimou tmimatos-grapse ston agwgo    
   if (write (pipefd[1], &c,1) != 1)  {//circbuffer[wrptr] = c;
     printf("Error writing 1 byte\n");
     exit(-1);
-  }
-
-  printf("write: %c\n", c);
+  }  
+  //telos krisimou tmimatos-auksise ton write-pointer
+  buff->wrptr = nxt_wr_ptr;
+  printf("write: %c, pos: %d\n", c,nxt_wr_ptr);
   return ;
 
 }
@@ -72,6 +72,7 @@ void pipe_write(char c)  {
 
 int pipe_read(void)  {
   char c;  
+  //arxi krisimou tmimatos
   while (1) {
     if (buff->wrptr != buff->rdptr)  { //if empty      
       break;
@@ -82,8 +83,9 @@ int pipe_read(void)  {
     exit(-1);
   }
   
+  //telos krisimou kwdika - auksise ton read-pointer
   buff->rdptr = (buff->rdptr+1)%buff->size;
-  printf("read: %c\n", c );
+  printf("read: %c, pos: %d\n", c,buff->rdptr);
 
   
   //kalese tin pipe_close() gia kleisimo tou agwgou
@@ -95,7 +97,7 @@ int pipe_read(void)  {
 
 void *thread_reader()  {
    int readval;
-   printf("ok1\n");
+   
    while(1){
       readval = pipe_read();
       if (readval == 0)  {
@@ -107,11 +109,11 @@ void *thread_reader()  {
 }
 
 
-
-void *thread_writer()  {   
+void writer() {
+   
    char  ch='A';
    int i = 0;
-   printf("ok2\n");
+   
    while(1){
      pipe_write(ch+i);    
      i = (i+1)%24;//Letter of alphabet in ordermax numbers in alphabet is 24.    
@@ -122,24 +124,33 @@ void *thread_writer()  {
 
 
 int main(int argc, char **argv)  {
-  pthread_t tid1, tid2;
+  pthread_t tid1;
   
   pipe_init(BUF_SIZE);  
   if (pthread_create(&tid1,NULL,thread_reader,NULL) != 0) {
     printf("Error creating reader thread\n");
     exit(-1);
   }
-    
+  
+  writer();  
+    /*
   if (pthread_create(&tid2,NULL,thread_writer,NULL) != 0) {
     printf("Error creating writer thread\n");
     exit(-1);
-  }  
+  }  */
   
   
   //writer();  
    
-  pthread_join(tid1,NULL);
-  pthread_join(tid2,NULL);
+  if (pthread_join(tid1,NULL) != 0) {
+    printf("error at read thread\n");
+    exit(-1);
+  }
+  /*
+  if (pthread_join(tid2,NULL) != 0) {
+    printf("error at write thread\n");
+    exit(-1);
+  }*/
  
 
   return 0;
