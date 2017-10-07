0c054b8faa706fc30935137435a27c0db34c9c07
trakaros <caxelos@inf.uth.gr>
2017-10-07 13:02:37 +0300

    while loop sync

--- askisi1.c
+++ askisi1.c
@@ -16,7 +16,6 @@ bufferT buff = NULL;
 int pipefd[2];
 
 void pipe_init(int size)  {
-
   buff = (bufferT)malloc( sizeof(bufferT) );
   if (buff == NULL) {
     printf("Error allocating memory\n");
@@ -27,19 +26,11 @@ void pipe_init(int size)  {
   buff->wrptr=0;
 
 
-/*  
-  circbuffer = (char *)malloc( size*sizeof(char) );
-  if (circbuffer == NULL)  {
-    printf("Error allocating mem\n");
-    return ;
-  }
-*/
   if ( pipe(pipefd) == -1)  {
     printf("Error at pipe\n");
     exit(-1);  
   }
-  
-  
+ 
 
 }
 
@@ -55,37 +46,20 @@ void pipe_close()  {
   }
 }
 
-int pipe_read(char *c)  {
-   
-  if (buff->wrptr == buff->rdptr)  { //if empty      
-    //perimene mexri na graftoun data
-  }
-  
-  if (1 != read (pipefd[0], c, 1) ) {
-    printf("Error reading 1 Byte\n");
-    return -1;
-  }
-  
-  buff->rdptr = (buff->rdptr+1)%buff->size;
-  printf("read: %c\n", *c );
-  
-  
-  //kalese tin pipe_close() gia kleisimo tou agwgou
-  
-  return 1;  
-}
-
-
 void pipe_write(char c)  {
+ 
   int nxt_wr_ptr = (buff->wrptr + 1)%buff->size;
-  
-  if ( nxt_wr_ptr == buff->rdptr )  {//check if it is full
-    //perimene mexri na diavastoun dedomena
-  } 
-  
-  buff->wrptr = nxt_wr_ptr;
-  //circbuffer[wrptr] = c;
-  if (write (pipefd[1], &c,1) != 1)  {
+
+ //vres pou prepei na valeis ta race conditions  
+  while (1)  {
+    if ( nxt_wr_ptr != buff->rdptr )  {//check if it is full
+      buff->wrptr = nxt_wr_ptr;
+      break;
+    }
+    printf("write: thread waiting\n");
+  }   
+    
+  if (write (pipefd[1], &c,1) != 1)  {//circbuffer[wrptr] = c;
     printf("Error writing 1 byte\n");
     exit(-1);
   }
@@ -95,46 +69,78 @@ void pipe_write(char c)  {
 
 }
 
-/*
-void *thread_reader()  {
-   while(1){
-      char    ch;
-      int     result;
 
-      result = read (pipefd[0],&ch,1);
-      if (result != 1) {
-        perror("read");
-        exit(3);
-      }
-      
-      printf ("Reader: %c\n", ch);   
-   }
+int pipe_read(void)  {
+  char c;  
+  while (1) {
+    if (buff->wrptr != buff->rdptr)  { //if empty      
+      break;
+    }
+  }
+  if (1 != read (pipefd[0], &c, 1) ) {
+    printf("Error reading 1 Byte\n");
+    exit(-1);
+  }
+  
+  buff->rdptr = (buff->rdptr+1)%buff->size;
+  printf("read: %c\n", c );
+
+  
+  //kalese tin pipe_close() gia kleisimo tou agwgou
+
+  
+  return 1;  
 }
 
-*/
-void *thread_writer()  {
+
+void *thread_reader()  {
+   int readval;
+   printf("ok1\n");
+   while(1){
+      readval = pipe_read();
+      if (readval == 0)  {
+        return NULL;
+      }   
+   }
+
+ 
+}
+
+
+
+void *thread_writer()  {   
    char  ch='A';
    int i = 0;
+   printf("ok2\n");
    while(1){
-     pipe_write(ch+i); 
-   
+     pipe_write(ch+i);    
      i = (i+1)%24;//Letter of alphabet in ordermax numbers in alphabet is 24.    
    }
+
 }
+
 
 
 int main(int argc, char **argv)  {
+  pthread_t tid1, tid2;
   
-  pipe_init(BUF_SIZE);
-  pthread_t  tid1, tid2;
+  pipe_init(BUF_SIZE);  
+  if (pthread_create(&tid1,NULL,thread_reader,NULL) != 0) {
+    printf("Error creating reader thread\n");
+    exit(-1);
+  }
+    
+  if (pthread_create(&tid2,NULL,thread_writer,NULL) != 0) {
+    printf("Error creating writer thread\n");
+    exit(-1);
+  }  
   
-/*
-  pthread_create(&tid1,NULL,thread_reader,NULL);
-  pthread_create(&tid2,NULL,thread_writer,NULL);
-
+  
+  //writer();  
+   
   pthread_join(tid1,NULL);
   pthread_join(tid2,NULL);
-*/  
+ 
 
   return 0;
 
