bbade651dd24f9f44241ceda7a7fd51fcab8b9cb
trakaros <caxelos@inf.uth.gr>
2017-10-07 20:29:18 +0300

    ask1, energi anamoni

--- askisi1.c
+++ askisi1.c
@@ -2,8 +2,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
 
 #define BUF_SIZE 10
+#define MAX_EMPTY_TIMES 100
+#define MAX_WRITE_TIMES 1000
 
 struct buffinfo {
   int size;
@@ -12,8 +15,14 @@ struct buffinfo {
 };
 typedef struct buffinfo *bufferT;
 
+
+const char text[150] = "I must send this text correctly. If it hasn't been sent in order there are problems in sychronization\n"; 
+char final_text[150] = "\0";
 bufferT buff = NULL;
 int pipefd[2];
+volatile int join_threads;
+
+
 
 void pipe_init(int size)  {
   buff = (bufferT)malloc( sizeof(bufferT) );
@@ -46,25 +55,25 @@ void pipe_close()  {
   }
 }
 
-void pipe_write(char c)  {
- 
+void pipe_write(char c)  { 
   int nxt_wr_ptr = (buff->wrptr + 1)%buff->size;
 
   while (1)  {
     if ( nxt_wr_ptr != buff->rdptr )  {//check if it is full
       break;
     }
-    printf("write: thread waiting\n");
+    printf("write: buffer is full\n");
   }   
   
   //arxi krisimou tmimatos-grapse ston agwgo    
   if (write (pipefd[1], &c,1) != 1)  {//circbuffer[wrptr] = c;
-    printf("Error writing 1 byte\n");
+    printf("pipe_write:Error writing 1 byte\n");
     exit(-1);
   }  
   //telos krisimou tmimatos-auksise ton write-pointer
   buff->wrptr = nxt_wr_ptr;
   printf("write: %c, pos: %d\n", c,nxt_wr_ptr);
+ 
   return ;
 
 }
@@ -72,11 +81,17 @@ void pipe_write(char c)  {
 
 int pipe_read(void)  {
   char c;  
+  int empty_times=0;
   //arxi krisimou tmimatos
   while (1) {
     if (buff->wrptr != buff->rdptr)  { //if empty      
       break;
     }
+    empty_times++;
+    printf("read: buffer is empty.%d times\n", empty_times);
+    if (empty_times == MAX_EMPTY_TIMES) 
+      return 0;
+   
   }
   if (1 != read (pipefd[0], &c, 1) ) {
     printf("Error reading 1 Byte\n");
@@ -86,10 +101,7 @@ int pipe_read(void)  {
   //telos krisimou kwdika - auksise ton read-pointer
   buff->rdptr = (buff->rdptr+1)%buff->size;
   printf("read: %c, pos: %d\n", c,buff->rdptr);
-
-  
-  //kalese tin pipe_close() gia kleisimo tou agwgou
-
+  final_text[ strlen(final_text) ] = c;  
   
   return 1;  
 }
@@ -101,6 +113,9 @@ void *thread_reader()  {
    while(1){
       readval = pipe_read();
       if (readval == 0)  {
+        printf("Destroy pipe\n");
+        pipe_close();
+        join_threads = 1;
         return NULL;
       }   
    }
@@ -109,14 +124,16 @@ void *thread_reader()  {
 }
 
 
-void writer() {
-   
-   char  ch='A';
+void writer() { 
    int i = 0;
    
    while(1){
-     pipe_write(ch+i);    
-     i = (i+1)%24;//Letter of alphabet in ordermax numbers in alphabet is 24.    
+     pipe_write(text[i]);    
+     i++;
+     if (text[i] == '\n') {
+       printf("write: Write limit reached\n"); 
+       return ;
+     }     
    }
 
 }
@@ -125,6 +142,7 @@ void writer() {
 
 int main(int argc, char **argv)  {
   pthread_t tid1;
+   
   
   pipe_init(BUF_SIZE);  
   if (pthread_create(&tid1,NULL,thread_reader,NULL) != 0) {
@@ -133,25 +151,18 @@ int main(int argc, char **argv)  {
   }
   
   writer();  
-    /*
-  if (pthread_create(&tid2,NULL,thread_writer,NULL) != 0) {
-    printf("Error creating writer thread\n");
-    exit(-1);
-  }  */
   
+  while (join_threads == 0) {
   
-  //writer();  
-   
+  }
+  /*
   if (pthread_join(tid1,NULL) != 0) {
     printf("error at read thread\n");
     exit(-1);
-  }
-  /*
-  if (pthread_join(tid2,NULL) != 0) {
-    printf("error at write thread\n");
-    exit(-1);
   }*/
- 
+  
+  printf("text: %s\n", text);
+  printf("final_text: %s\n", final_text);
 
   return 0;
 
