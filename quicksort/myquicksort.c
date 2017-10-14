f331bcd499cfe0f31af4de4741009e90d6806d0d
trakaros <caxelos@inf.uth.gr>
2017-10-15 01:17:47 +0300

    dont try this at home

--- quicksort/myquicksort.c
+++ quicksort/myquicksort.c
@@ -13,25 +13,82 @@ struct task {
   pthread_t tid;
   quickArgsT *args;
   struct task *next;
+  volatile int t;
+  volatile int taking;
+  volatile int number;
 };
 typedef struct task taskT;  
-
-
 
 int array[MAX_SIZE];
 taskT *taskHead = NULL;
 
-//int final_array[MAX_SIZE];
+
+int priority(int t, int number) {
+  taskT *curr;
+  
+  for (curr = taskHead; curr != NULL; curr=curr->next)  {
+    if (t == 0)
+      return 0;
+    else if (curr->t < t)
+      return 0;
+    else if (curr->t == t && curr->number < number)
+      return 0;    
+      
+  }  
+  printf("priority\n");
+  return 1; 
+}
+
+void bakery_algorithm(taskT *newtask)  {
+  taskT *curr;
+  
+  newtask->taking = 1;
+  newtask->t = -1;  
+  for (curr=taskHead; curr != NULL; curr=curr->next) {
+    if (curr->t > newtask->t)
+      newtask->t = curr->t;
+  }
+  newtask->t = newtask->t + 1;
+  newtask->taking = 0;
+  
+      
+  for (curr=taskHead; curr != NULL; curr=curr->next) {
+    while (curr->taking == 1) {
+    
+    }
+    while (priority(curr->t, curr->number) == 1) {
+      printf("waiting\n");
+     
+    }
+  }  
+  
+  
+}
+
+void delete_thread(taskT *delThread) {
+  taskT *curr;
+  
+  for (curr = head; curr != NULL; curr=curr->next) [
+    if (curr->next == delThread)  {
+      curr->next = delThread->next;
+      free( delThread );
+      return ;
+    }
+  } 
+
+
+
 
 void destroy_task_list(void) {
   taskT *curr = taskHead; 
   taskT *prev = NULL;
-  
-  for (curr = taskHead; curr != NULL; curr = curr->next ) {
+    
+  for (curr = taskHead; curr->next != NULL; curr = curr->next ) {
     prev = curr;
     curr = curr->next;
     free(prev);
   }
+  free(curr);
 }
 
 void insert_to_task_list(taskT *newtask)  {
@@ -49,12 +106,12 @@ void insert_to_task_list(taskT *newtask)  {
   
   
   return ;    
-    
-  
-
 }
 
+
 taskT *createTask(int left, int right)  {
+  static int numOfThreads = 0;  
+  
   quickArgsT *newArgs = NULL;
   taskT *newtask = NULL;
   
@@ -73,19 +130,29 @@ taskT *createTask(int left, int right)  {
   }    
   newtask->args = newArgs;
   newtask->next = NULL;  
-
-
+  newtask->taking = 0;
+  newtask->t = 0;
+  newtask->number = numOfThreads;
+  
+  numOfThreads++;
+  bakery_algorithm(newtask);
   //arxi krisimou
   insert_to_task_list(newtask);
   //telos krisimou
+  newtask->t = 0;
+  
+  
   return newtask;
 }
 
 //void *myquicksort(int *array, int left, int right )  {
 void *myquicksort(void *args) {
+  //take left or right node
   taskT *mytask = (taskT *)args;
+  printf("called\n");
   
-  int i = mytask->args->left;//left-1;
+  //init counters
+  int i = mytask->args->left-1;//left-1;
   int j = mytask->args->right;
   int o = array[mytask->args->right];
   int temp;
@@ -93,7 +160,7 @@ void *myquicksort(void *args) {
   taskT *newLeftTask = NULL;
   taskT *newRightTask = NULL;
 
-  printf("ok");    
+      
   while (1)  {
     while ( array[++i] < o )  {    
     }  
@@ -115,35 +182,31 @@ void *myquicksort(void *args) {
 
   
   if (mytask->args->left < i-1)  {
-    
+    printf("inside\n");    
     newLeftTask = createTask(mytask->args->left, i-1);
-    if (newLeftTask == NULL)
+    if (newLeftTask == NULL) {
       return NULL;
-      
+    }      
     if (pthread_create( &(newLeftTask->tid), NULL, (void *)myquicksort, (void *)newLeftTask) != 0)  {
       printf("Error creating left thread. Exiting\n");
       return NULL;
     }
   }
-  else {
-    return NULL;
-  }  
-    
+      
   if (i+1 < mytask->args->right)  {
+    printf("inside\n");
     newRightTask = createTask(i+1, mytask->args->right);
-    if (newRightTask == NULL)
+    if (newRightTask == NULL) {
       return NULL;
-  
+    }  
+   
     if ( pthread_create( &(newRightTask->tid), NULL, (void *)myquicksort, (void *)newRightTask) != 0 ) {
       printf("Error creating right thread. Exiting\n");
       return NULL;
     }
       
   }
-  else {
-    return NULL;
-  }
-
+ 
   return NULL;
 }
 
@@ -174,7 +237,7 @@ int main(int argc, char *argv[])  {
     printf("%d, ", array[i]);
   }
   putchar('\n');
-  destroy_task_list();
+  //destroy_task_list();
   
   return 0;
 }
