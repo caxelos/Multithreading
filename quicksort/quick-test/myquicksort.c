c018764308cbaac42fd01e1e0730060414c88c25
trakaros <caxelos@inf.uth.gr>
2017-10-15 17:23:45 +0300

    trexei_swsta

--- quicksort/quick-test/myquicksort.c
+++ quicksort/quick-test/myquicksort.c
@@ -14,7 +14,6 @@ typedef struct quickArgs quickArgsT;
 struct task {
   pthread_t tid;
   quickArgsT *args;
-  struct task *next;
 };
 typedef struct task taskT;  
 
@@ -22,51 +21,6 @@ int array[MAX_SIZE];
 taskT *taskHead = NULL;
 
 
-void destroy_task(taskT *destroyTask) {
-  static int i=0;
-  i++;
-  printf("%da\n", i);
-  taskT *curr = NULL; 
-  taskT *prev = NULL;
-
-  if (destroyTask == taskHead)  {
-    free( taskHead->args );
-    free( taskHead );
-    return ;
-  }
-     
-  prev = taskHead;
-  curr = taskHead->next;
-  while (1)  {
-    if (curr == destroyTask)  {
-      prev->next = curr->next;
-      free( curr->args );
-      free( curr );
-      curr = NULL;
-      printf("%db\n", i);
-      return ;
-    }
-    prev = curr;
-    curr = curr->next;
-  } 
-
-  return ;
-
-}
-
-//tin xreiazomai gt alliws den mporw na kanw destroy
-void insert_to_task_list(taskT *newtask)  {
-  taskT *currtask;
-  
-  currtask = taskHead;
-  while (currtask->next != NULL) {
-    currtask = currtask->next;
-  }
-  currtask->next = newtask;
-
-  return ;
-}  
- 
 
 taskT *createTask(int left, int right)  {
   quickArgsT *newArgs = NULL;
@@ -86,24 +40,14 @@ taskT *createTask(int left, int right)  {
     return NULL;
   }    
   newtask->args = newArgs;
-  newtask->next = NULL;  
  
-
-
-  //arxi krisimou
-  //insert_to_task_list(newtask);
-  //telos krisimou
-  
-  
   return newtask;
 }
 
-//void *myquicksort(int *array, int left, int right )  {
+
 void *myquicksort(void *nextTask) {
   static int numOfThreads=1;
-  static int created = 1;
-  static int destroyed = 0;
-  //take left or right node
+
   taskT *mytask = (taskT *)nextTask;
   taskT *rTask = NULL;
   taskT *lTask = NULL;
@@ -135,15 +79,13 @@ void *myquicksort(void *nextTask) {
 
    
   if (mytask->args->left < i-1)  {
-    numOfThreads++;
-    created++;     
+    numOfThreads++;     
     lTask = createTask(mytask->args->left, i-1);
     if (lTask == NULL) {
       return NULL;
     }
     
-  
-    //printf("createdThread:(%d,%d)\n", mytask->lTask->args->left, mytask->lTask->args->right);     
+     
     if (pthread_create( &(lTask->tid), NULL, (void *)myquicksort, (void *)lTask) != 0)  {     
       printf("Error creating left thread. Exiting\n");
       return NULL;
@@ -156,10 +98,9 @@ void *myquicksort(void *nextTask) {
     if ( rTask == NULL) {
       return NULL;
     }
-    created++;
+
      
  
-    //printf("createdThread:(%d,%d)\n", mytask->rTask->args->left, mytask->rTask->args->right);
     if ( pthread_create( &( rTask->tid), NULL, (void *)myquicksort, (void *) rTask) != 0 ) {
       printf("Error creating right thread. Exiting\n");
       return NULL;
@@ -167,7 +108,7 @@ void *myquicksort(void *nextTask) {
       
   }
 
-   //printf("finishedThread:(%d,%d)\n", mytask->args->left, mytask->args->right);
+
   if (mytask == taskHead) {
     while ( numOfThreads != 1) {
       if (sched_yield() != 0) {
@@ -175,26 +116,52 @@ void *myquicksort(void *nextTask) {
         return NULL;
       }
     }
-    printf("created:%d, destroyed:%d\n", created, destroyed);
   }
 
-  //printf("FREEThread:(%d,%d)\n", mytask->args->left, mytask->args->right);
   free( mytask->args );
   free( mytask );
- 
+  numOfThreads--;
 
   return NULL;
 }
 
+
+int read_number()  {
+  int i = 0;
+  char number_str[7];
+
+  while (1) {  
+    number_str[i] = getchar();
+    if (number_str[i] == ' ')
+      continue;
+    else if (number_str[i] == '\n')  {
+      number_str[i] = '\0';
+      break;
+    }
+    else
+      i++;
+  }
+
+   return atoi(number_str);
+}
+
+
 int main(int argc, char *argv[])  {
   quickArgsT *args;
-  int i;
+  int i=0, numOfElements;
   
+ 
+  
+  printf("Please number of elements<enter>: ");
+  numOfElements = read_number();
+
   //init array
-  for (i = 0; i < MAX_SIZE; i++)  {
-    array[i] = i%10;  
+  for (i = 0; i < numOfElements; i++)  {
+    printf("no.%d: ", i+1);
+    array[i] = read_number();
   } 
   
+   
   //init main thread
   taskHead = (taskT *)malloc( sizeof(taskT) );
   if (taskHead == NULL)
@@ -207,15 +174,13 @@ int main(int argc, char *argv[])  {
     return -1;
   }
   args->left = 0;
-  args->right = MAX_SIZE-1;
+  args->right = numOfElements-1;//MAX_SIZE-1;
   taskHead->args = args; 
  
   myquicksort(taskHead);
 
 
-  //print final array
-
-  for (i = 0; i < MAX_SIZE; i++)  {
+  for (i = 0; i < numOfElements; i++)  {
     printf("%d, ", array[i]);
   }
   putchar('\n');
